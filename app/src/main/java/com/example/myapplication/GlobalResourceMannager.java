5f73d20f1d998e617b352cd282e230d5b9445967
ChinaSSS <2247135671@qq.com>
2019-05-31 13:38:41 +0800

    create add record activity

---
+++ app/src/main/java/com/example/myapplication/GlobalResourceMannager.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.content.Context;
+
+public class GlobalUtil {
+    private static final String TAG = "GlobalUtil";
+
+    private Context context;
+    private RecordDBHelper recordDBHelper;
+
+    public static GlobalUtil globalUtil;
+
+
+
+}
